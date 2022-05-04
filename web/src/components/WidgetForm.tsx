e701bf0adf449f2957613e5d208cb06872f6a6ed
NicolasMaso <nicolas.masopb@Hotmail.com>
2022-05-03 23:18:28 -0300

    Initiated WidgetForm

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,16 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from "@headlessui/react";
+
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixei seu feedback</span>
+      </header>
+
+      <p> Hello World</p>
+
+      <footer>Feito com ♥ pela Rocketseat</footer>
+    </div>
+  );
+}
